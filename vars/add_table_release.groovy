7714b71a3afdd56588800cdb0fa2eebbe5ee3f6a
f3r <f3r73ch@gmail.com>
2020-12-20 23:13:31 +0000

    Reset error count between rows. herpderp

--- vars/add_table_release.groovy
+++ vars/add_table_release.groovy
@@ -3,12 +3,7 @@ def call(){
 	withCredentials([usernameColonPassword(credentialsId: 'confluence-f3r73ch', variable: 'USERPASS')]) {
 		sh '''
 		  set +x
-		  curl -s \
-			-u "$USERPASS" \
-			--request PUT \
-			--header 'Accept: application/json' \
-			-d@update-confluence.json \
-			https://f3r73ch.atlassian.net/wiki/rest/api/content/1540097
+		  curl -s -u "$USERPASS" --request PUT --header 'Accept: application/json' -d@update-confluence.json https://f3r73ch.atlassian.net/wiki/rest/api/content/1540097
 		'''
 
 	}
