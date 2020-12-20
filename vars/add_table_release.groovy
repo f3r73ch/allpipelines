d0fce0fd04829c052746a44083502da50b7765f0
f3r <f3r73ch@gmail.com>
2020-12-20 23:30:46 +0000

    just trolling the repo

--- vars/add_table_release.groovy
+++ vars/add_table_release.groovy
@@ -2,14 +2,19 @@ def call(){
 	echo "here"
 
 	withCredentials([usernameColonPassword(credentialsId: 'confluence-f3r73ch', variable: 'USERPASS')]) {
-		sh '''
-		  curl \
-		  -u "$USERPASS" \
-		  --request PUT \
-		  --header 'Accept: application/json' \
-		  -d@update-confluence.json \
-		  https://f3r73ch.atlassian.net/wiki/rest/api/content/1540097
-		'''
+		def response = sh(
+							script: '''
+									  curl \
+									  -u "$USERPASS" \
+									  --request PUT \
+									  --header 'Accept: application/json' \
+									  -d@update-confluence.json \
+									  https://f3r73ch.atlassian.net/wiki/rest/api/content/1540097
+									''', 
+							returnStdout: true
+		)
+
+		echo response
 
 	}
 }
