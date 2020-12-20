f7d577f437071ba33715479f2f4ba7847de9d9f4
f3r <f3r73ch@gmail.com>
2020-12-20 23:15:12 +0000

    Don't Ask Me, I Have No Idea Why This Works Either

--- vars/add_table_release.groovy
+++ vars/add_table_release.groovy
@@ -3,7 +3,12 @@ def call(){
 	withCredentials([usernameColonPassword(credentialsId: 'confluence-f3r73ch', variable: 'USERPASS')]) {
 		sh '''
 		  set +x
-		  curl -s -u "$USERPASS" --request PUT --header 'Accept: application/json' -d@update-confluence.json https://f3r73ch.atlassian.net/wiki/rest/api/content/1540097
+		  curl \
+		  -u "$USERPASS" \
+		  --request PUT \
+		  --header 'Accept: application/json' \
+		  -d@update-confluence.json \
+		  https://f3r73ch.atlassian.net/wiki/rest/api/content/1540097
 		'''
 
 	}
